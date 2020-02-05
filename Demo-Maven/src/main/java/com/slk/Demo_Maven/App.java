bbaec28769115a8bb6d57459445208fcb2ee08af
Tarun patel <tarunpatel2982@gmail.com>
2020-02-06 00:20:48 +0530

    05-02-2020

--- Demo-Maven/src/main/java/com/slk/Demo_Maven/App.java
+++ Demo-Maven/src/main/java/com/slk/Demo_Maven/App.java
@@ -1,13 +1,35 @@
 package com.slk.Demo_Maven;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		 ObjectMapper mapper = new ObjectMapper();
+	        String json = "{\"name\":\"mkyong\", \"age\":\"37\"}";
+
+	        try {
+
+	            // convert JSON string to Map
+	            Map<String, String> map = mapper.readValue(json, Map.class);
+
+				// it works
+	            //Map<String, String> map = mapper.readValue(json, new TypeReference<Map<String, String>>() {});
+
+	            System.out.println(map);
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
     }
 }
