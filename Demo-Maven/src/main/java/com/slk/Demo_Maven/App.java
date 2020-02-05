103b4c796b412f9268bac9c1bfaee07db30c8464
Tarun patel <tarunpatel2982@gmail.com>
2020-02-05 23:59:29 +0530

    05-02-2020

---
+++ Demo-Maven/src/main/java/com/slk/Demo_Maven/App.java
@@ -0,0 +1,13 @@
+package com.slk.Demo_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
